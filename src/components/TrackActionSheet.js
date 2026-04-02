96e457747b65e9b2f6934610bebd28921e4036c1
ProjectFeelz <steve@projectfeelz.com>
2026-04-02 23:54:58 +0200

    Update TrackActionSheet.js

--- src/components/TrackActionSheet.js
+++ src/components/TrackActionSheet.js
@@ -12,7 +12,7 @@ import ShareCard from './ShareCard';
 import {
     X, Share2, ListMusic, Download, Heart, Play, Music, Loader, Check,
     ChevronLeft, ShoppingCart, Lock, PlusCircle, DollarSign, Clock,
-} from 'lucide-react';
+}
 
 const PAYPAL_CLIENT_ID = process.env.REACT_APP_PAYPAL_CLIENT_ID;
 
