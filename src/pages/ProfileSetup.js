afe5b33ed06c1343cd0904a2d0c6bd8637ae97ba
ProjectFeelz <steve@projectfeelz.com>
2026-09-08 23:09:21 +0000

    json

--- src/pages/ProfileSetup.js
+++ src/pages/ProfileSetup.js
@@ -27,7 +27,7 @@ import { useStreakContext } from '../contexts/StreakContext';
 import {
   SOCIALS, GENRES_LIST, MOODS_LIST, ARTIST_TABS,
   PROFILE_IMAGE_BUCKET, MAX_DAILY_THOUGHTS, THOUGHT_TTL_MS, BIO_MAX,
-} from './profile/profileShared';
+} from './profileShared';
 
 export default function ProfilePage() {
   const nav = useNavigate();
